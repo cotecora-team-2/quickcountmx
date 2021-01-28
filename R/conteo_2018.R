1a5c72cace0b8e96b78d52ff26a5ea13ee960273
Felipe Gonzalez <felipexgonzalez@gmail.com>
2021-01-27 21:21:17 -0600

    Temporary documentation

--- R/conteo_2018.R
+++ R/conteo_2018.R
@@ -2,10 +2,5 @@
 #'
 #' Contains results of vote counts for the 2018 presidential election in Mexico
 #'
-#' @format A data frame with 156840 rows and 95 variables:
-#' \describe{
-#'   \item{CLAVE_CASILLA - JAMK}{computos distritales 2018}
-#'   \item{TVIVPARHAB-VPH_SNBIEN}{2010 INEGI census data}
-#' }
 #' @source \url{https://computos2018.ine.mx/#/presidencia/nacional/1/1/1/1}
 "conteo_2018"
