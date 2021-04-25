969a38becdea506380272d07e18a8750bfc25041
ixime <ixmezach@googlemail.com>
2021-04-25 01:10:39 -0500

    script y marco para simulacro1

---
+++ R/process_election_day.R
@@ -0,0 +1,106 @@
+write_results <- function(fit, file_name, team, tot_estratos, n_estratos, tot_casillas, n_casillas,
+                          path_out, path_results){
+  EN <- stringr::str_sub(file_name, 10, 11)
+  R <- stringr::str_sub(file_name, 12, 17)
+
+  tab_candidatos <- fit$estimates %>%
+    dplyr::mutate(across(where(is.numeric), round, 2)) %>%
+    dplyr::arrange(desc(median)) %>% dplyr::select(party,median,inf,sup) %>% filter(party != "OTROS") %>%
+    tibble::column_to_rownames(var="party") %>%
+    tibble::rownames_to_column() %>%
+    tidyr::gather(LMU, value, -rowname) %>%
+    tidyr::spread(rowname, value) %>% dplyr::rename(PART = part) %>% dplyr::mutate(LMU = dplyr::case_when(
+      LMU == "inf" ~ 0,
+      LMU == "median" ~ 1,
+      LMU == "sup" ~ 2
+    ),
+    LMU = as.integer(LMU),
+    EQ = team,
+    EN = EN,
+    R = R ) %>%
+    relocate(c(EQ,EN,R), .before = everything()) %>%
+    relocate(c(PART,LMU), .after = last_col())
+
+  tab_compulsados <- tab_candidatos %>%
+    mutate(ESTRATOS = ifelse(LMU == 0,tot_estratos,""),
+           EST_REC = ifelse(LMU == 0,n_estratos,""),
+           TOT_CAS = ifelse(LMU == 0,tot_casillas,""),
+           CAS_REC = ifelse(LMU == 0,n_casillas,""),
+           PORCENTAJE = ifelse(LMU == 0,round(n_casillas/tot_casillas, digits = 2),""))
+
+
+
+  readr::write_csv(tab_candidatos, file = paste0(path_out, "/", team,
+                                               EN, R, ".csv"))
+  readr::write_csv(tab_compulsados, file = paste0(path_results, "/", "compulsado",
+                                                 EN, R, ".csv"))
+}
+#' @param path_name Path to a file that will be used for estimation. On election
+#' day it will be a file with a subset of the sample.
+#' @param file_name Name of the file with the data.
+#' @param path_results Path to the directory where partial results will be
+#' saved.
+#' @param path_out Path to directory where diagnostics, partial results, and
+#' data will be saved. Data is included in case there is need of additional
+#' checks.
+#' @param team Name of team running the model, to be used in INE reports.
+#' @inheritParams mrp_estimation
+#'
+#' @rdname process_batch_election_day
+#' @export
+process_batch <- function(path_name, file_name, path_out, path_results,
+                          team = "default", n_iter = 4000, n_burnin = 3000, n_chains = 1,
+                          parallel = TRUE){
+  print(team)
+  tipo <- stringr::str_sub(file_name, 8, 9)
+  estado_str <- stringr::str_sub(file_name, 10, 11)
+
+  table_frame <- readr::read_rds("data-raw/marco_2021.rds")
+  table_frame <- table_frame %>%
+    ungroup() %>%
+    mutate(ln = ifelse(LISTA_NOMINAL_CASILLA==0, 1200, LISTA_NOMINAL_CASILLA)) %>%
+    filter(ID_ESTADO == as.numeric(estado_str)) %>%
+    mutate(CLAVE_CASILLA = gsub("'","",CLAVE_CASILLA))
+
+  candidatos <- readr::read_csv("data-raw/estados_candidatos_partidos_2021.csv") %>%
+    filter(ID_ESTADO == as.numeric(estado_str))
+  lista_candidatos <- candidatos$CANDIDATO %>% unique()
+
+  data_in <- readr::read_delim(path_name, "|", escape_double = FALSE,
+                               trim_ws = TRUE, skip = 1) %>%
+    mutate(ID_ESTADO = iD_ESTADO) %>%
+    mutate(CLAVE_CASILLA = paste0(stringr::str_pad(ID_ESTADO, 2, pad = "0"),
+                                  stringr::str_pad(SECCION, 4, pad = "0"),
+                                  TIPO_CASILLA,
+                                  stringr::str_pad(ID_CASILLA, 2, pad = "0"),
+                                  stringr::str_pad(EXT_CONTIGUA,2,pad="0"))) %>%
+    filter(!is.na(PAN))
+  print(paste0("datos: ", path_name))
+  print(paste0("salidas: ", path_out))
+  # do processing ########
+  muestra_m <- left_join(data_in, table_frame, by=c("CLAVE_CASILLA")) %>%
+    mutate(estrato = as.character(estrato))
+  data_stratum_tbl <- table_frame %>%
+    filter(ID_ESTADO==as.numeric(estado_str)) %>%  count(estrato) %>%
+    mutate(estrato = as.character(estrato))
+
+  tot_estratos <- nrow(data_stratum_tbl)
+  n_estratos <- muestra_m %>% select(estrato) %>% unique() %>% nrow()
+  tot_casillas <- table_frame %>% nrow()
+  n_casillas <- data_in %>% nrow()
+
+  # run model ###################
+  fit_time <- system.time(
+    fit <- hb_estimation(muestra_m, stratum = estrato, id_station = no_casilla,
+                          sampling_frame = table_frame,
+                          parties = all_of(lista_candidatos),
+                          covariates = comp_marg_imp, num_iter = 300, chains = 4, part = TRUE)
+  )
+  print(fit_time)
+
+  write_results(fit = fit, file_name = file_name,
+                team = team, tot_estratos = tot_estratos, n_estratos = n_estratos,
+                tot_casillas, n_casillas,
+                path_out = path_out,
+                path_results = path_results)
+}
---
+++ scripts/monitor.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+# First argument is path to data files
+# Second argument is time in seconds between checks of new files
+# (Optional) if third argument is 1, then start by running last REMESAS file
+
+import sys, getopt
+import os
+import time
+import subprocess
+import argparse
+
+def procesar_nombre(filename):
+  descriptors = {'tipo':'Desconocido'}
+  try:
+    nombre, ext = filename.split(".")
+    if(ext=='txt'):
+      codigo = nombre[-10:]
+      tipo = nombre[:-10]
+      eleccion_tipo = codigo[:2]
+      id_estado = codigo[2:4]
+      fecha = codigo[4:10]
+      descriptors = {'nombre':nombre, 'tipo':tipo,
+          'eleccion_tipo':eleccion_tipo,
+          'id_estado':id_estado,
+          'fecha':fecha}
+  except Exception as ex:
+    print("Error procesando archivo")
+
+  return descriptors
+
+def main(params):
+  if not os.path.exists(params.path_out):
+    os.makedirs(params.path_out)
+  if not os.path.exists(params.path_results):
+    os.makedirs(params.path_results)
+  print("Observando " + params.data_path + " cada "+ str(params.wait_sec) + " segundos.")
+  files_before = [f for f in os.listdir(params.data_path) if f[:7] == "REMESAS"]
+  files_before.sort()
+  if params.last:
+    files_before = files_before[:-1]
+  while 1:
+    files_now = [f for f in os.listdir(params.data_path) if f[:7] == "REMESAS"]
+    added = [f for f in files_now if not f in files_before]
+    if added:
+      print(".")
+      added.sort()
+      print("Se agregaron: ", ",".join(added))
+      # use last one
+      added = added[-1:]
+      for filename in added:
+        descriptores = procesar_nombre(filename)
+        print(descriptores)
+        if(descriptores["tipo"] == "REMESAS"):
+          full_path = os.path.join(params.data_path, filename)
+          subprocess.call(["r", "-e", "quickcountmx:::process_batch('" +full_path+"','"+descriptores['nombre']+"','"+params.path_out+"','"+params.path_results+"','"+params.team+"')"])
+          subprocess.call(["sed","-i",'s/,,,,,$//g',params.path_results+'/compulsado'+descriptores['id_estado']+descriptores['fecha']+'.csv'])
+    else:
+      print('.', end = '', flush = True)
+    files_before = files_now
+    time.sleep(int(params.wait_sec))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Quickcountmx monitor')
+
+    # Experiment setup params
+    parser.add_argument("--data_path", "-dp", type=str, default="default1",
+                        help="A folder with this name would be created to dump saved models and log files")
+    parser.add_argument("--path_out", "-po", type=str,
+                        help="Data path of output")
+    parser.add_argument("--path_results", "-pr", type=str,
+                        help="Path of results")
+    parser.add_argument('--wait_sec', "-s", type=int,
+                        help="Wait seconds")
+    parser.add_argument('--team', "-t", type=str, default="default",
+                        help="Team name")
+    parser.add_argument('--last', '-l', type=bool, default=False,
+                        help='whether it is the last one')
+
+    params = parser.parse_args()
+
+    main(params)
