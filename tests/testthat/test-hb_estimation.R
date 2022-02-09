42e8ed835ea8c6ef7075cf866c44b07fa5535647
Felipe Gonzalez <felipexgonzalez@gmail.com>
2022-02-08 18:29:03 -0600

    Set separate config and tests

--- tests/testthat/test-hb_estimation.R
+++ tests/testthat/test-hb_estimation.R
@@ -41,3 +41,18 @@ test_that("test call", {
   expect_equal(nrow(estimates), 3)
   expect_lt(mean(abs(estimates$median - c(5/16, 10/16, 1/16))), 0.05)
 })
+
+test_that("test call corr", {
+  sample_tbl <-
+    select_sample_prop(test_tbl, stratum = state, frac = 0.3, seed = 912)
+  fit <- hb_estimation(sample_tbl, stratum = state,
+                       sampling_frame = test_tbl,
+                       prop_obs = 0.9, seed = 12,
+                       model = "mlogit-corr",
+                       parties = cand_1:otro, covariates = x1:x_2,
+                       num_iter = 100, chains = 1)
+  estimates <- fit$estimates
+  expect_is(estimates, "tbl")
+  expect_equal(nrow(estimates), 3)
+  expect_lt(mean(abs(estimates$median - c(5/16, 10/16, 1/16))), 0.05)
+})
