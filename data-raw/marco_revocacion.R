4838ac7512c72db66a877f7e4f64a5d5877a35b5
Felipe Gonzalez <felipexgonzalez@gmail.com>
2022-03-20 22:47:17 -0600

    Change name of sampling frame

---
+++ data-raw/marco_revocacion.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+
+marco_rev_tmp <- read_csv("../pruebas-conteo/reporte-votacion-sim/votacion_simulada_simulacro.csv")
+
+marco_revocacion <- marco_rev_tmp |>
+  select(CLAVE_CASILLA:ESTRATO) |>
+  mutate(ln = LISTA_NOMINAL) |>
+  mutate(ln = ifelse(ln == 0, 3100, ln)) |>
+  mutate(x = as.numeric(TIPO_CASILLA == "S")) |>
+  mutate(id_casilla = row_number()) |>
+  mutate(estrato = ID_ESTRATO) 
+
+write_csv(marco_revocacion, file = "data-raw/marco_revocacion.csv")
