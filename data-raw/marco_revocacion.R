bc52f3752b1f898f63105a5367920647e4041711
Felipe Gonzalez <felipexgonzalez@gmail.com>
2022-03-26 12:49:30 -0600

    Agregar marco de simulacro 2

--- data-raw/marco_revocacion.R
+++ data-raw/marco_revocacion.R
@@ -1,14 +1,14 @@
 library(tidyverse)
 
 
-marco_rev_tmp <- read_csv("../pruebas-conteo/reporte-votacion-sim/votacion_simulada_simulacro1_OK.csv")
+marco_rev_tmp <- read_csv("../pruebas-conteo/reporte-votacion-sim/votacion_simulada_simulacro_2.csv")
 
 marco_revocacion <- marco_rev_tmp |>
-  select(CLAVE_CASILLA:ESTRATO) |>
+  select(CLAVE_CASILLA:ID_ESTRATO) |>
   mutate(ln = LISTA_NOMINAL) |>
-  mutate(ln = ifelse(ln == 0, 3100, ln)) |>
+  mutate(ln = ifelse(ln == 0, 3050, ln)) |>
   mutate(x = as.numeric(TIPO_CASILLA == "S")) |>
   mutate(id_casilla = row_number()) |>
-  mutate(estrato = ID_ESTRATO) 
+  mutate(estrato = ID_ESTRATO)
 
 write_csv(marco_revocacion, file = "data-raw/marco_revocacion.csv")
