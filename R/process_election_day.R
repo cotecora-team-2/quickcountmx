2a04f53dbea89516552551edfd07d7c114d5c1da
Felipe Gonzalez <felipexgonzalez@gmail.com>
2022-04-11 19:00:19 -0500

    Afinaciones para día de revocación

--- R/process_election_day.R
+++ R/process_election_day.R
@@ -121,13 +121,13 @@ process_batch <- function(path_name, file_name, log_file, path_out, path_mailbox
   n_muestra_m <- muestra_m %>% nrow()
   logger::log_info(paste0("numero de casillas despues de union con marco: ", n_muestra_m))
 
-  prop_obs <- n_muestra_m / 1820
+  prop_obs <- n_muestra_m / 1830
 
   # run model ###################
   fit_time <- system.time(
     fit <- hb_estimation(muestra_m, stratum = estrato, id_station = id_casilla,
                          sampling_frame = table_frame,
-                         parties = all_of(lista_opciones), prop_obs = 0.9 * prop_obs,
+                         parties = all_of(lista_opciones), prop_obs = 0.8 * prop_obs,
                          covariates = all_of(c("x")), num_iter = as.numeric(n_iter),
                          chains = as.numeric(n_chains), seed = as.numeric(seed),
                          adapt_delta = as.numeric(adapt_delta),
--- data-raw/marco_revocacion.R
+++ data-raw/marco_revocacion.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 
-marco_rev_tmp <- read_csv("../pruebas-conteo/reporte-votacion-sim/votacion_simulada_simulacro_2.csv")
+marco_rev_tmp <- read_csv("./data-raw/votacion_simulada_simulacro_2.csv")
 
 marco_revocacion <- marco_rev_tmp |>
   select(CLAVE_CASILLA:ID_ESTRATO) |>
@@ -11,4 +11,9 @@ marco_revocacion <- marco_rev_tmp |>
   mutate(id_casilla = row_number()) |>
   mutate(estrato = ID_ESTRATO)
 
-write_csv(marco_revocacion, file = "data-raw/marco_revocacion.csv")
+modificaciones_ultimas <- read_csv("./data-raw/ubicacion_casillas_final_08042022.csv") |>
+  select(CLAVE_CASILLA)
+
+marco_revocacion_ajustado <- semi_join(marco_revocacion, modificaciones_ultimas)
+
+write_csv(marco_revocacion_ajustado, file = "data-raw/marco_revocacion.csv")
