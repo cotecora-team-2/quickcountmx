2184b46fbb54f9fe7391107e69df88b2975351df
ammadrigal <ammadrigal@users.noreply.github.com>
2024-03-21 22:15:03 -0600

    Clean files and create roxygen documentation

--- R/fns_codigo_diputados.R
+++ R/fns_codigo_diputados.R
@@ -1,78 +1,119 @@
-#### Primer intento de reescribir codigo de LE en funciones
+#### First attempt to rewrite LE code in functions
 
-## calcular_nd1: Calcula la reparticion de los primeros 300 diputados por maximo relativo.
+
+#' @name asignar_diputados
+#' @aliases calcular_nd1
+#' @aliases calcular_nd2
+#' @aliases corregir_nd2_sobrerep
+#'
+#' @title  Functions that help with the  assignment of the Chamber of Deputies.
+#'
+#' @description There are 500 seats to be assigned in the Chamber of Deputies in Mexico.
+#' The process involves different steps.
+#' `calcular_nd1`: Calculates the assignment of the first 300 seats by "maximo relativo".
+#' `calcular_nd2`: Calculates the assignment of the second 200 seats by "representacion proporcional".
+#' `corregir_nd2_sobrerep`: Corrects the second assignment of the second 200 seats for over representation,
+#'  taking into account the maximum number allowed per party.
+#'
+#' @param theta_long a data frame in long format with columns:
+#' `id_estrato` (integer for each of the 300 federal districts);
+#' `id_partido` (integer for each of the parties);
+#' `sim` (integer id for each of the simulations);
+#' `value` (numeric with the value of the estimated theta)
+#' @param coa_long tibble in long format with the data for coalition assignment in each Federal District
+#' `id_estrato` (integer for each of the 300 federal districts);
+#' `coaliciones_que_aplican` (character, with coalitions that apply in the DF,
+#' either name of coalition to match `coalicion_partido` in `keys_part_coa`),
+#' "Ambas" if both/all or "Ninguna" if no coalitions apply for that DF;
+#' `nombre_partido` (character with party name);
+#' `reciben_votos_coa` (integer, indicator: 1 if the party of the coalition receives the votes for that DF, 0 if not);
+#' @param keys_part_coa a data frame with the key variables for assignment.
+#' `id_partido` (integer for each of the parties);
+#' `nombre_partido` (character with party name);
+#' `candidato_independiente` (numeric, 1 if the party is from an independent candidate, 0 if not);
+#' `id_coalicion` (numeric, id for coalicion);
+#' `coalicion_partido` (character, name of the coalicion);
+#' @param lista_nominal_estrato a data frame with the nominal list for each DF. Columns:
+#' `id_estrato` (integer id for each of the 300 federal districts);
+#' `LISTA_NOMINAL` (integer, nominal list for each of the 300 federal districts);
+#' @param eta_long tibble in long format with with `sim`, `id_partido`, `nombre_partido` and
+#' `eta` which is created in `calcular_nd2` and used in `corregir_nd2_sobrerep` as an input.
+#' @param nd1_long tibble in long format with grouped by simulation with columns:
+#' `sim`, `id_partido`, `nombre_partido`, and `nd1`.
+#'  `nd1_long` is created in `calcular_nd1` and used in `corregir_nd2_sobrerep` as an input
+#' @param nd2_1_long tibble in long format with grouped by simulation with columns:
+#' `sim`, `id_partido`, `nombre_partido`, and `nd2_1`.
+#'  `nd2_long` is created in `calcular_nd2` and used in `corregir_nd2_sobrerep` as an input
+#'
+#' @rdname asignar_diputados
+#' @return  `calcular_nd1` returns a tibble (`nd1_long`) grouped by simulation with columns:
+#' `sim`, `id_partido`, `nombre_partido`, and
+#' `nd1` (integer, number of seats out of the first 300 assigned for each of the parties);
+#'
+#' @export
 calcular_nd1 <- function(theta_long,coa_long,keys_part_coa){
-  
-  ## Asignar votos acorde a coa (cada DF decide a que partido se le asignan los votos a la coalicion)
+
+  ## Assign votes depending on coalition (each DF decides which party is assigned the coalition votes)
   votos_asignacion_coa <- theta_long |>
-    #filter(id_partido %in% keys_part_coa$id_partido) |> ## dejar solo los partidos hasta CI1, el right_join se hace cargo de eso
     right_join(keys_part_coa) |>
     arrange(sim,id_estrato,id_partido) |>
     left_join(coa_long, by=c("id_estrato","nombre_partido")) |>
-    # calcular sumas por coalicion: variable auxiliar
     group_by(sim,id_estrato,coalicion_partido) |>
-    mutate(suma_coalicion = sum(value)) |> 
-    ## repartir por coalicion
+    mutate(suma_coalicion = sum(value)) |> # calculate sums by coalition: auxiliar variable
     group_by(sim,id_estrato) |>
     mutate(valor_ajustado = case_when(
       coalicion_partido == coaliciones_que_aplican ~ suma_coalicion*reciben_votos_coa,
-      (coaliciones_que_aplican == "Ambas") ~ suma_coalicion*reciben_votos_coa, 
-      TRUE ~ value)) |> # view()
+      (coaliciones_que_aplican == "Ambas") ~ suma_coalicion*reciben_votos_coa,
+      TRUE ~ value)) |>  #view()
     select(id_estrato,id_partido,sim,nombre_partido,valor_ajustado)
-  
-  ## sacar pid = partido con el maximo por estrato
+
+  ## calculate for each simulation and DF which party got the maximum
   partido_maximo_DF_sim <- votos_asignacion_coa |>
-    ## para cada simulacion y estrato cual partido obtuvo el maximo
     group_by(sim,id_estrato) |>
     summarise(partido_con_maximo = id_partido[which.max(valor_ajustado)])
-  
-  ## nd1 numero de diputaciones en el primer paso (maximo relativo)
+
+  ## nd1: number of deputies after first step (maximo relativo)
+  ## for each simulation number of times that each party was maximum in the 300 DF
   nd1_long <- partido_maximo_DF_sim |>
-    ## para cada simulacion numero de veces que el partido fue maximo en las 300 DF
     group_by(sim,partido_con_maximo) |>
     summarise(nd1 = n()) |>
     select(sim,id_partido = partido_con_maximo,nd1) |>
     left_join(keys_part_coa |> select(id_partido,nombre_partido)) |>
     select(sim,id_partido,nombre_partido,nd1)|>
     arrange(sim,id_partido)
-  
+
   return(nd1_long)
 } # end function
-
-### Primera vuelta nd2 
+#'
+#' @rdname asignar_diputados
+#' @return  `calcular_nd2` returns a list with 2 elements:
+#' `nd2_1_long` is a tibble grouped by simulation with columns:
+#' `sim`, `id_partido`, `nombre_partido` and
+#' `nd2_1` (numeric, number of seats out of the first assignment of the 200 seats assigned for each of the parties);
+#' and `eta_long` another tibble with `sim`, `id_partido`, `nombre_partido` and `eta` which is used in the next function for corrections.
+#'
+#' @export
 calcular_nd2_1 <- function(theta_long,lista_nominal_estrato,keys_part_coa){
-  #Creacion de lambda 
-  ## Los nulos se quitan .. primero 11 partidos... y luego participacion... 
-  ## En el codigo de LE theta tiene info para participacion. 
-  ## Nosotros lo hacemos separado.
-  
-  lam_long <- theta_long |>
+
+  # lambda partidos
+  lambda_partidos <- theta_long |>
     left_join(lista_nominal_estrato) |>
     group_by(sim,id_partido) |>
-    summarise(lam = sum(value*LISTA_NOMINAL/sum(LISTA_NOMINAL)))
-  
-  # lambda partidos
-  lambda_partidos <- lam_long |>
-    filter(id_partido %in% keys_part_coa$id_partido) |> ## dejar solo los partidos hasta CI1
+    summarise(lam = sum(value*LISTA_NOMINAL/sum(LISTA_NOMINAL))) |>
+    filter(id_partido %in% keys_part_coa$id_partido) |>
     group_by(sim) |>
-    mutate(lambda = lam/sum(lam)) |> 
+    mutate(lambda = lam/sum(lam)) |>
     select(-lam)
-  
-  # calcular theta3
+
+  # calculate theta3
   theta3_long <- theta_long |>
-    # quitar a candidato independiente y votos nulos
-    filter(id_partido %in% keys_part_coa$id_partido[keys_part_coa$candidato_independiente ==0]) |> 
+    # take out independent candidates
+    filter(id_partido %in% keys_part_coa$id_partido[keys_part_coa$candidato_independiente ==0]) |>
     left_join(lambda_partidos) |>
-    mutate(value = ifelse(lambda > 0.03, value, 0)) |> # quitar partidos con votacion menor al 3%
-    ## pregunta: la lambda se calcula con el partido independiente, pero aqui se quita el independiente, es correcto?
+    mutate(value = ifelse(lambda > 0.03, value, 0)) |> # take out parties with votes less than 3%
     select(-lambda)
-  
-  #check ... See PES con remesa "0400020027"
-  #lambda_partidos |> filter(lambda <= 0.03)
-  #theta3_long %>% filter(value == 0)
-  
-  # calcular eta.
-  # similar a lam pero con theta3
+
+  # calculate eta.
   eta_long <- theta3_long |>
     left_join(lista_nominal_estrato) |>
     group_by(sim,id_partido) |>
@@ -80,13 +121,8 @@ calcular_nd2_1 <- function(theta_long,lista_nominal_estrato,keys_part_coa){
     group_by(sim) |>
     mutate(eta = eta/sum(eta)) |>
     left_join(keys_part_coa |> select(id_partido,nombre_partido))
-  
-  #Asignacion de diputados por rep. proporcional
-  # Primer calculo de nd2
-  #nd2<-floor(200*eta) 
-  # rtot<-200-apply(nd2,1,sum)
-  # mm<-t(apply(rm2,1,order,decreasing=TRUE)) ## orden de mayor a menor
-  
+
+  # Asignacion de diputados por rep. proporcional. Primer calculo de nd2
   nd2_1_long <- eta_long |>
     group_by(sim) |>
     mutate(nd2_1a = floor(200*eta)) |>
@@ -97,29 +133,31 @@ calcular_nd2_1 <- function(theta_long,lista_nominal_estrato,keys_part_coa){
     mutate(nd2_1 = if_else(aux,nd2_1a+1,nd2_1a)) |>
     select(sim,id_partido,nombre_partido,nd2_1) |>
     arrange(sim,id_partido)
-  
+
   return(list(nd2_1_long=nd2_1_long,eta_long=eta_long))
 } # end funcion calcular nd2_1
-
-### Maximo numero de diputaciones y sobrerrepresentacion
+#'
+#' @rdname asignar_diputados
+#' @return  `corregir_nd2_sobrerep` uses outputs from the other two functions and returns a tibble with columns:
+#' `sim`, `id_partido`, `nombre_partido` and
+#' `nd2` (numeric, number of seats out of the corrections for over representation of the 200 seats assigned for each of the parties);
+#'
+#' @export
 corregir_nd2_sobrerep <- function(eta_long,nd1_long,nd2_1_long){
-  
-  max_nd_partido <- eta_long |> ### possible issue... eta es para 10 partidos. Que onda con el independiente? 
+
+  max_nd_partido <- eta_long |>
     mutate(max_nd = floor(500*(eta+0.08))) |>
     select(-eta)
-  
+
   nd2_sobrep_long <- nd2_1_long
-  # # crear un caso donde si haya sobrerrep
+  # # In case it is needed to create a case where there is sobrerrep in LE example
   # nd2_sobrep_long <- nd2_1_long |>
   #   mutate(nd2_1 = if_else(sim==1&id_partido==7,nd2_1+28,nd2_1)) |>
-  #   mutate(nd2_1 = if_else(sim==1&id_partido==1,nd2_1-28,nd2_1)) 
+  #   mutate(nd2_1 = if_else(sim==1&id_partido==1,nd2_1-28,nd2_1))
   eta2_long <- eta_long
-  # solo sobre 10 primeros partidos
-  ##  LE hace los siguientes dos pasos 3 veces arbitrariamente... adivino pensando que depues de 3 vueltas se ajustan las nd2
-  # meter un while
   stop = FALSE
   while(!stop){
-    print(stop)
+    #print(stop)
     sobrerrep <-  nd2_sobrep_long|>
       left_join(nd1_long) |>
       select(sim, id_partido, nombre_partido,nd1,nd2_1) |>
@@ -128,32 +166,29 @@ corregir_nd2_sobrerep <- function(eta_long,nd1_long,nd2_1_long){
       mutate(nd = nd1 + nd2_1) |>
       mutate(id_sobrerrep= nd > max_nd) |>
       mutate(cuantas_sobran = if_else(id_sobrerrep,nd-max_nd,0))
-    
-    ## parar de hacerlo si no hay ningun caso de sobre-representacion? 
-    # algo asi como:
+
     if(sum(sobrerrep$id_sobrerrep) >0) {
-      ## copiar logica de LE para entender
-      ## volver a repartir proporcionalmente los que sobran... 
+      ## this follows LE logic. Might be done better.
       rep_prop_sr1 <- sobrerrep |>
         mutate(nd2_aj1 = if_else(id_sobrerrep,max_nd-nd1,nd2_1))|>
         mutate(pr = if_else(id_sobrerrep,0,max_nd-nd))|>
-        mutate(pi = if_else(pr>0,1,0))|>
-        #Correccion por rep.prop. negativa
+        mutate(piid = if_else(pr>0,1,0))|>
+        #Correction for negative rep.prop.
         mutate(nd2_aj2 = if_else(nd2_aj1<0,0,nd2_aj1))|>
         mutate(pr_aj1 = if_else(pr<0,0,pr))|>
-        # segundo paso para repartir proporcionalmente
+        # second step to assign proportions
         group_by(sim) |>
-        mutate(rtot_aj1 = 200-sum(nd2_aj2*(1-pi)))|>
+        mutate(rtot_aj1 = 200-sum(nd2_aj2*(1-piid)))|>
         left_join(eta2_long)|>
-        mutate(eta2 = pi*eta/sum(pi*eta)) |>
-        mutate(nd2_aj3 = nd2_aj2*(1-pi)+floor(rtot_aj1*eta2)) |>
-        mutate(rm2_aj1 = pi*(rtot_aj1*eta2-floor(rtot_aj1*eta2))) |>
+        mutate(eta2 = piid*eta/sum(piid*eta)) |>
+        mutate(nd2_aj3 = nd2_aj2*(1-piid)+floor(rtot_aj1*eta2)) |>
+        mutate(rm2_aj1 = piid*(rtot_aj1*eta2-floor(rtot_aj1*eta2))) |>
         mutate(rtot_aj2 = 200-sum(nd2_aj3)) |>
         arrange(sim,desc(rm2_aj1))|>
         mutate(aux = row_number()<=rtot_aj2)|>
         mutate(nd2_2 = if_else(aux,nd2_aj3+1,nd2_aj3))|>
         arrange(sim,id_partido)
-      
+
       nd2_sobrep_long <- rep_prop_sr1 |>
         select(sim,id_partido,nombre_partido,nd2_1 = nd2_2)
       eta2_long <- rep_prop_sr1 |>
@@ -162,8 +197,8 @@ corregir_nd2_sobrerep <- function(eta_long,nd1_long,nd2_1_long){
       stop = TRUE
     } # else
   } # while
-  
+
   nd2_sobrep_long <- nd2_sobrep_long |> rename(nd2 = nd2_1)
-  
+
   return(nd2_sobrep_long)
 } # end funcion corregir
--- tests/testthat/test-LEcodigoDiputados.R
+++ tests/testthat/test-LEcodigoDiputados.R
@@ -1,11 +1,10 @@
-#### Primer intento de reescribir codigo de LE en funciones
-### Este test replica el codigo de LE y checa que las funciones den los resultados esperados
+## This tests replicates LE's code and checks that functions give expected results.
 
 #library(tidyverse)
 #source("./R/fns_codigo_diputados.R")
 
-## crear un archivo con Keys para partidos y coaliciones
-## ahorita hardcodeado... hay que ver de donde se saca esta info.
+## create an object with Keys for parties and coalitions
+## at the moment hardcoded...
 keys_part_coa <- tibble(id_partido = 1:11,
                         nombre_partido = c("PAN","PRI","PRD","PT","PVEM","MC","MORENA","PES","RSP","FPM","CI1"),
                         candidato_independiente = c(rep(0,10),1),
@@ -16,43 +15,38 @@ keys_part_coa <- tibble(id_partido = 1:11,
     TRUE ~ "sin_coalicion"))
 
 ### READ inputs ####
-
 ## remesas y sus thetas
-dir <- "data_diputados_LE"
+dir <- "testdata_diputados"
 id_remesa<-"0400020027"
 
-testthat::test_path("data_diputados_LE")
-
 c1<-scan(testthat::test_path(dir,paste("REMESAS",id_remesa,".txt",sep="")),n=1)
-#c1<-scan(paste(dir,"REMESAS",id_remesa,".txt",sep=""),n=1)
 remesa<-read.table(testthat::test_path(dir,paste("REMESAS",id_remesa,".txt",sep="")),
                    header=TRUE,skip=1,sep="|")
 
 ## reads first 10 simulations for theta for id_remesa
-#theta_long<-read_rds(paste0(dir,"theta_melt_10_",id_remesa,".rds"))
 theta_long <- readr::read_rds(testthat::test_path(dir,paste0("theta_melt_10_",id_remesa,".rds")))
 
-
-## info = Info con numero de estrato para cada uno de los 300 DF y sus listas nominales...
+## info = Info with ID_ESTRATO for each of the 300 DF and their listas nominales...
 info<-read.csv(testthat::test_path(dir,"Info_distritos_2021.txt"))
+
 #Info coaliciones
-#coa<-read.csv("./data-raw/diputados_LE/2021_Diputados_coaliciones.csv")
 coa<-read.csv(testthat::test_path(dir,"2021_Diputados_Coaliciones.csv"))
-## reformatear coa
+
+## reformat coa to long object
 coa_long <- coa |>
   left_join(info|> select(ID_ENTIDAD = ID_ESTADO, DISTRITO = ID_DISTRITO,id_estrato =ID_ESTRATO)) |>
   select(id_estrato,coaliciones_que_aplican=Coaliciones.que.aplican,any_of(keys_part_coa$nombre_partido)) |>
   tidyr::pivot_longer(cols = any_of(keys_part_coa$nombre_partido),names_to = "nombre_partido",values_to = "reciben_votos_coa") |>
-  ## formatear nombres a que cuadren con el keys_part_coa
+  ## format names so they match keys_part_coa
   ## NB. FORMATEO MANUAL
   mutate(coaliciones_que_aplican = ifelse(coaliciones_que_aplican =="Va por M\xe9xico","Va por Mexico",coaliciones_que_aplican))
+
+### First Step: 300 seats Maximo Relativo  ####
 
 nd1_long<-calcular_nd1(theta_long,coa_long,keys_part_coa)
 #write_rds(nd1_long, file= paste0(dir,"nd1_long_10_0400020027.rds"))
 
-# para pruebas acorde a Felipe voy a fijar 1 simulaciones
-# se podria hacer igual para todas sin seleccionar una sola...
-#sim_check = 1:10
+# to test faster only one simulation. Could be done for all.  #sim_check = 1:10
 sim_check = 10
 theta_long_test<- theta_long |>
   filter(sim %in% sim_check)
@@ -66,25 +60,21 @@ testthat::test_that("nd1 checks", {
   check <- nd1_long_test |>
     group_by(sim) |>
     summarise(total = sum(nd1))  |>
-    filter(total !=300) ## cada simulacion reparte los 300 distritos
+    filter(total !=300) ## each simulation assigns 300 DFs
 
   testthat::expect_equal(nrow(check), 0)
   testthat::expect_equal(nd1_long_test, nd1_long_test_against)
 })
 
-### Para Representacion Proporcional ####
+### Second Step: 200 seats Rep Prop  ####
 lista_nominal_estrato <- info |> select(id_estrato = ID_ESTRATO, LISTA_NOMINAL)
 
-## Regresamos a theta para calcular la lambda de LE
-
-### Primera vuelta nd2
 nd2_1_calc<-calcular_nd2_1(theta_long,lista_nominal_estrato,keys_part_coa)
+
 nd2_1_long <- nd2_1_calc$nd2_1_long
 #write_rds(nd2_1_long, file= paste0(dir,"nd2_1_long_10_0400020027.rds"))
 
-# para pruebas acorde a Felipe voy a fijar 1 simulaciones
-# se podria hacer igual para todas sin seleccionar una sola...
-#sim_check = 1:10
+# to test faster only one simulation. Could be done for all.  #sim_check = 1:10
 sim_check = 10
 theta_long_test<- theta_long |>
   filter(sim %in% sim_check)
@@ -98,19 +88,13 @@ testthat::test_that("nd2_1 checks", {
   check <- nd2_1_long_test |>
     group_by(sim) |>
     summarise(total = sum(nd2_1))  |>
-    filter(total !=200) ## cada simulacion reparte los 200 distritos de representacion proporcional
+    filter(total !=200) ## each simulation assigns 200 DFs
 
   testthat::expect_equal(nrow(check), 0)
   testthat::expect_equal(nd2_1_long_test, nd2_1_long_test_against)
 })
 
-# check
-
-### Maximo numero de diputaciones y sobrerrepresentacion
-# Artículo 17, numerales 2 y 3, de la LGIPE; y, 54, fracciones IV y V de la CPEUM:
-#   Un PPN no podrá contar con un número de diputaciones por ambos principios que representen un porcentaje del total de la Cámara que exceda en ocho puntos a su porcentaje de votación nacional emitida.
-# A partir de lo anterior, se suma el número de diputaciones que le corresponden a cada PPN por ambos principios y se verifica si existe sobrerrepresentación:
-# pm<-floor(500*(eta+0.08)) # PPT slide 7. Num maximo de diputados
+### Third Step: Corrections for 200 seats Rep Prop  ####
 
 eta_long <- nd2_1_calc$eta_long
 
@@ -120,12 +104,13 @@ testthat::test_that("nd2_corregida checks", {
   check <- nd2_corregida |>
     group_by(sim) |>
     summarise(total = sum(nd2))  |>
-    filter(total !=200) ## cada simulacion reparte los 200 distritos de representacion proporcional
+    filter(total !=200)  ## each simulation assigns 200 DFs
 
   testthat::expect_equal(nrow(check), 0)
 
 })
 
+### Total 500 seats summing up nd1 and nd2  ####
 
 nd <- nd1_long |> full_join(nd2_corregida) |>
   arrange(sim,id_partido) |>
@@ -136,7 +121,7 @@ testthat::test_that("nd checks", {
   check <- nd |>
     group_by(sim) |>
     summarise(total = sum(nd))  |>
-    filter(total !=500) ## cada simulacion reparte los 500 distritos total
+    filter(total !=500)  ## each simulation assigns 500 DFs
 
   testthat::expect_equal(nrow(check), 0)
 
