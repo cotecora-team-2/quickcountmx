0d5a85393d499627f92ca5f8cb52da3684d84c2a
Michelle Anzarut <anzarutm@hotmail.com>
2024-04-11 17:03:27 -0400

    add global variables

--- R/quickcountmx-package.R
+++ R/quickcountmx-package.R
@@ -16,3 +16,14 @@ utils::globalVariables(c("strata", "n_strata", "n_h", "n_votes", "party", "n_aux
 utils::globalVariables(c("state_abbr", "time", "huso", "lista_nominal_log",
                   "sim_time_nc", "max_time", "status_sim", "CLAVE_CASILLA",
                   "time_obs_sim"))
+
+utils::globalVariables(c(".draw", "CANDIDATO", "CNR", "EQ", "EXT_CONTIGUA",
+                         "ID_CASILLA", "ID_ESTADO",
+                         "LISTA_NOMINAL", "LISTA_NOMINAL_CASILLA", "LMU",
+                         "NULOS", "PART", "SECCION",
+                         "TIPO_CASILLA", "TOTAL", "cand_id", "data",
+                         "estrato", "head", "inf", "internal_id", "median",
+                         "no_casilla", "part", "pivot_longer", "prop_votes",
+                         "quantile", "rowname", "std_error",
+                         "sup", "total", "total_ln_str", "total_nominal",
+                         "total_votes_str", "votes", "votos"))
