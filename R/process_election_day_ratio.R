9b20e0c0956b6b8e7bf6781ab3821e562788ea4e
Felipe Gonzalez <felipexgonzalez@gmail.com>
2024-04-21 10:27:07 -0600

    Update paths process ratio

--- R/process_election_day_ratio.R
+++ R/process_election_day_ratio.R
@@ -60,14 +60,14 @@ ratio_process_batch <- function(path_name, file_name, path_out, B,
   tipo <- stringr::str_sub(file_name, 8, 9)
   estado_str <- stringr::str_sub(file_name, 10, 11)
 
-  table_frame <- readr::read_rds("data-raw/marco_2021.rds")
+  table_frame <- readr::read_rds("data-raw/marco_2024.rds")
   table_frame <- table_frame %>%
     ungroup() %>%
     mutate(ln = LISTA_NOMINAL) %>%
     filter(ID_ESTADO == as.numeric(estado_str)) %>%
     mutate(CLAVE_CASILLA = gsub("'","",CLAVE_CASILLA))
 
-  candidatos <- readr::read_csv("data-raw/estados_candidatos_partidos_2021.csv") %>%
+  candidatos <- readr::read_csv("data-raw/estados_candidatos_partidos_2024.csv") %>%
     filter(ID_ESTADO == as.numeric(estado_str))
   lista_candidatos <- candidatos$CANDIDATO %>% unique()
 
