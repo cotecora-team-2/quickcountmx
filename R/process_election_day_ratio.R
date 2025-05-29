15a98b75a636593acd24a0baf42e556399e546ca
Felipe Gonzalez <felipexgonzalez@gmail.com>
2025-05-29 17:11:43 -0600

    Delete simulation sample file, and do not filter

--- R/process_election_day_ratio.R
+++ R/process_election_day_ratio.R
@@ -87,9 +87,9 @@ ratio_process_batch <- function(path_name, file_name, path_out, B,
     #filter(TOTAL > 0)
 
   ######## Leer y filtrar muestra ###########################
-  muestra_seleccionada <- readr::read_rds("data-raw/muestra_2025.rds") |>
-    mutate(CLAVE_CASILLA = gsub("'","",CLAVE_CASILLA))
-  data_in <- data_in |> semi_join(muestra_seleccionada)
+  #muestra_seleccionada <- readr::read_rds("data-raw/muestra_2025.rds") |>
+  #  mutate(CLAVE_CASILLA = gsub("'","",CLAVE_CASILLA))
+  #data_in <- data_in |> semi_join(muestra_seleccionada)
   #####################################################
   excluir_tbl <- readr::read_csv("data-raw/casillas_excluidas.csv")
   data_in <- data_in |> anti_join(excluir_tbl)
